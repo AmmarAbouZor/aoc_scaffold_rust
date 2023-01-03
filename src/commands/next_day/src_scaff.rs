e631288209522b504dc80ec6a1585907f6e5e9b5
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-01-03 20:45:25 +0100

    append day implemented

--- src/commands/next_day/src_scaff.rs
+++ src/commands/next_day/src_scaff.rs
@@ -1,5 +1,5 @@
-use anyhow::Context;
-use anyhow::Result;
+use anyhow::{bail, Context, Result};
+use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex;
 use regex::Regex;
@@ -24,10 +24,6 @@ fn get_year_file_name(year: &str) -> String {
     format!("{}.rs", get_year_dir_name(year))
 }
 
-fn append_next_day(src_path: &PathBuf, year: &str) -> Result<u8> {
-    todo!()
-}
-
 fn create_year(src_path: &PathBuf, year: &str) -> Result<u8> {
     println!("year '{year}' doesn't exist yet. creating year '{year}'...");
 
@@ -36,7 +32,7 @@ fn create_year(src_path: &PathBuf, year: &str) -> Result<u8> {
 
     generate_day_file(&year_dir_path, 1)?;
     generate_year_file(src_path, year)?;
-    add_year_main(src_path, year)?;
+    insert_year_main(src_path, year)?;
     Ok(1)
 }
 
@@ -44,7 +40,7 @@ fn get_day_name(day: u8) -> String {
     format!("day_{:02}", day)
 }
 
-fn generate_day_file(dir_path: &PathBuf, day: u8) -> Result<()> {
+fn generate_day_file(year_dir_path: &PathBuf, day: u8) -> Result<()> {
     lazy_static! {
         static ref DAY_TEMPLATE: &'static str = r"fn part_1() {}
 fn part_2() {}
@@ -63,7 +59,7 @@ mod test {
 }";
     }
 
-    let file_path = dir_path.join(get_day_name(day)).with_extension("rs");
+    let file_path = year_dir_path.join(get_day_name(day)).with_extension("rs");
     fs::write(file_path, DAY_TEMPLATE.clone())?;
 
     Ok(())
@@ -95,7 +91,7 @@ fn run_day(day: u8) {
     Ok(())
 }
 
-fn add_year_main(dir_path: &PathBuf, year: &str) -> Result<()> {
+fn insert_year_main(dir_path: &PathBuf, year: &str) -> Result<()> {
     let main_path = dir_path.join("main.rs");
     let mut text = fs::read_to_string(main_path.clone())?;
 
@@ -158,5 +154,88 @@ fn add_year_main(dir_path: &PathBuf, year: &str) -> Result<()> {
     }
 
     fs::write(main_path, text)?;
+    Ok(())
+}
+
+fn append_next_day(src_path: &PathBuf, year: &str) -> Result<u8> {
+    let year_dir_path = src_path.join(get_year_dir_name(year));
+
+    let last_day_num = get_last_day(&year_dir_path)?;
+    let next_day = last_day_num + 1;
+    generate_day_file(&year_dir_path, next_day)?;
+
+    let year_file_path = year_dir_path.with_extension("rs");
+    insert_day_in_year(&year_file_path, next_day)?;
+    Ok(next_day)
+}
+
+pub fn get_last_day(year_dir_path: &PathBuf) -> Result<u8> {
+    // filename of the days follow the pattern day_XX.rs
+    const DAY_FILE_LENGTH: usize = 9;
+
+    let last_day = fs::read_dir(year_dir_path)?
+        .flatten()
+        .map(|file_path| file_path.file_name().into_string())
+        .flatten()
+        .filter(|file| {
+            file.starts_with("day_") && file.ends_with(".rs") && file.len() == DAY_FILE_LENGTH
+        })
+        .sorted()
+        .last()
+        .context("Years folder exists but it's empty")?;
+
+    let last_day_num = last_day[4..6].parse().context(format!(
+        "filename {last_day} doesn't follow the pattern day_XX.rs"
+    ))?;
+
+    Ok(last_day_num)
+}
+
+fn insert_day_in_year(year_file_path: &PathBuf, day: u8) -> Result<()> {
+    let mut text = fs::read_to_string(year_file_path.clone())?;
+
+    // insert mod
+    let insert_mod_index = {
+        if let Some(last_entry) = text.rfind("mod day_") {
+            let mut insert_index = text[last_entry..]
+                .find(";")
+                .context("year content is changed outside of this program")?;
+
+            insert_index += last_entry;
+
+            text.insert(insert_index + 1, '\n');
+
+            insert_index + 2
+        } else {
+            text.insert_str(0, "\n\n");
+            0
+        }
+    };
+
+    let day_mod = format!("mod {};", get_day_name(day));
+
+    text.insert_str(insert_mod_index, &day_mod);
+
+    // insert in run functions
+    if let Some(_) = text.find("run_day(") {
+        lazy_static! {
+            static ref DAY_FUNC_REGEX: Regex = Regex::new(r#"run_day\(.\);"#).unwrap();
+        }
+
+        let run_func_with_arg = format!("run_day({day});");
+        text = DAY_FUNC_REGEX.replace(&text, run_func_with_arg).into();
+
+        // day match
+        let insert_index_match = text
+            .find(r#"_ => unreachable!("day not implemented")"#)
+            .context("year file content is changed outside of this program")?;
+
+        let day_match = format!("{day} => {}::run(),\n        ", get_day_name(day));
+        text.insert_str(insert_index_match, &day_match);
+    } else {
+        bail!("year file content is changed outside of this program")
+    }
+
+    fs::write(year_file_path, text)?;
     Ok(())
 }
